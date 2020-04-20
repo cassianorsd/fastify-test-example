b653e928aa0cb44bad315d04dd2050ceaba11a85
cassianorsd <cassiano.rsd@gmail.com>
2020-04-20 11:53:44 -0300

    fix

---
+++ server.js
@@ -0,0 +1,13 @@
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.get('/', (request, reply) => {
+  reply.type('application/json').code(200)
+  return { hello: 'world' }
+})
+
+fastify.listen(8000,'0.0.0.0', (err, address) => {
+  if (err) throw err
+  fastify.log.info(`server listening on ${address}`)
+})
