4ea873641d416bad68a1131dab9769948b9f4df6
cassianorsd <cassiano.rsd@gmail.com>
2020-04-20 11:54:31 -0300

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ fastify.get('/', (request, reply) => {
   return { hello: 'world' }
 })
 
-fastify.listen(8000,'0.0.0.0', (err, address) => {
+fastify.listen(21058,'0.0.0.0', (err, address) => {
   if (err) throw err
   fastify.log.info(`server listening on ${address}`)
 })
